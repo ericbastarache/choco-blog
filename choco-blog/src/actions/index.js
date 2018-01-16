6205d181ac593f237f0b8ad613950184820db533
Eric Bastarache <eric_bastarache@hotmail.com>
2018-01-15 21:37:48 -0500

    - added boilerplate for redux - created some reducers, actions
    - will need to refactor later to use redux saga but for now this is fine

---
+++ choco-blog/src/actions/index.js
@@ -0,0 +1,16 @@
+const FETCH_POSTS = 'FETCH_POSTS';
+const FETCH_POST = 'FETCH_POST';
+
+export const fetchPosts = (state) => {
+    return {
+        type: FETCH_POSTS,
+        payload: state
+    }
+}
+
+export const fetchPost = (id) => {
+    return {
+        type: FETCH_POST,
+        payload: id
+    }
+}
---
+++ choco-blog/src/reducers/reducer_posts.js
@@ -0,0 +1,25 @@
+import { FETCH_POSTS, FETCH_POST } from '../actions';
+
+const INITIAL_STATE = {
+    posts: [],
+    post: null
+}
+
+const posts = (state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case FETCH_POSTS:
+            return {
+                ...state,
+                action.payload
+            }
+        case FETCH_POST:
+            return {
+                ...state,
+                action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export default posts;
