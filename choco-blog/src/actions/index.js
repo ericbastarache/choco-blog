2d2d72f03c8155fc7d945a08f8f23f077c7c684f
Eric Bastarache <eric_bastarache@hotmail.com>
2018-01-16 23:27:49 -0500

    - hopefully this action works???

--- choco-blog/src/actions/index.js
+++ choco-blog/src/actions/index.js
@@ -1,6 +1,9 @@
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 
+//local api url CHANGE LATER
+export const API_URL = 'http://localhost:5000/api';
+
 export const fetchPosts = (state) => {
     return {
         type: FETCH_POSTS,
@@ -9,8 +12,14 @@ export const fetchPosts = (state) => {
 }
 
 export const fetchPost = (id) => {
+
+    const response = fetch(`${API_URL}/posts/`, {
+        method: 'GET',
+        data: id
+    }).then(data => data);
+
     return {
         type: FETCH_POST,
-        payload: id
+        payload: data
     }
 }
